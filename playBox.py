994a4966e40e7bc6815859cead14feae84d9a41d
Alexandre Costa <68577176+alexandrekcosta@users.noreply.github.com>
2024-07-10 02:39:30 -0300

    Add files via upload

---
+++ playBox.py
@@ -0,0 +1,39 @@
+import pyautogui
+import time
+
+time.sleep(5)
+pyautogui.PAUSE = 0.5
+pyautogui.click(1797,49)
+pyautogui.PAUSE = 0.5
+pyautogui.click(768,398)
+pyautogui.PAUSE = 0.5
+pyautogui.write("amazoniacry@random.com.br")
+pyautogui.PAUSE = 0.5
+pyautogui.click(727,496)
+pyautogui.write("CricketLove")
+pyautogui.PAUSE = 0.5
+pyautogui.click(953,557)
+pyautogui.PAUSE = 0.5
+pyautogui.click(896,284)
+pyautogui.PAUSE = 0.5
+pyautogui.write("Chapéu")
+pyautogui.PAUSE = 0.5
+pyautogui.click(741,380)
+pyautogui.write("Chapeugate")
+pyautogui.PAUSE = 0.5
+pyautogui.click(756,481)
+pyautogui.write("Roupas")
+pyautogui.PAUSE = 0.5
+pyautogui.click(767,577)
+pyautogui.write("Roupas de novo :(")
+pyautogui.PAUSE = 0.5
+pyautogui.click(704,677)
+pyautogui.write("R$ 10")
+pyautogui.PAUSE = 0.5
+pyautogui.click(711,774)
+pyautogui.write("10 conto meu :(")
+pyautogui.PAUSE = 0.5
+pyautogui.click(715,864)
+pyautogui.write("Tô com preguiça :(")
+pyautogui.PAUSE = 0.5
+pyautogui.click(891,933)
